631a896f7ccbcfd73cf47621c2613ee80e0b0a05
mohankumar <mohankumar@mohan>
2017-02-28 11:38:59 +0530

    String Reverse

---
+++ ReserveString/src/com/mccoy/ReverseString.java
@@ -0,0 +1,11 @@
+package com.mccoy;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the String");
+		System.out.println("String is reversed");
+	}
+
+}
